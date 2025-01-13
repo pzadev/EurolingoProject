d3ba3f48e6ecff1292188596a4d709b9017bf644
Oksana Herasymenko <oksanagerasymenko@Mac.mynet>
2025-01-13 21:42:21 +0000

    fixing the .env and add on .gitignore the .env.local, we all should change it

--- backend/app.ts
+++ backend/app.ts
@@ -1,22 +1,39 @@
-import 'dotenv/config'
-import express from 'express'
-import apiRouter from './routers'
-import  { italianWords, frenchWords, germanWords, spanishWords, urkainianWords } from './data/devData/index'
-import { seeding } from './database/seeding'
-import { testFrenchWords, testGermanWords, testItalianWords, testSpanishWords, testUrkainianWords } from './data/testData/index'
+import express from "express";
+import apiRouter from "./routers";
+import {
+  italianWords,
+  frenchWords,
+  germanWords,
+  spanishWords,
+  urkainianWords,
+} from "./data/devData/index";
+import {
+  testFrenchWords,
+  testGermanWords,
+  testItalianWords,
+  testSpanishWords,
+  testUrkainianWords,
+} from "./data/testData/index";
+import { seeding } from "./database/seeding";
 
-const app = express()
-const port: number = 8080
+const app = express();
+const port: number = 8080;
 
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
 
-const bootstrap = async() => {
-    // await seeding( italianWords, frenchWords, germanWords, spanishWords, urkainianWords )
-    await seeding( testFrenchWords, testGermanWords, testItalianWords, testSpanishWords, testUrkainianWords )
-}
+const bootstrap = async () => {
+  // await seeding( italianWords, frenchWords, germanWords, spanishWords, urkainianWords )
+  await seeding(
+    testFrenchWords,
+    testGermanWords,
+    testItalianWords,
+    testSpanishWords,
+    testUrkainianWords
+  );
 
-app.listen(port, () => {
-    console.log('listening on port 8080')
-})
+  app.listen(port, () => {
+    console.log("listening on port 8080");
+  });
+};
 
-bootstrap()
+bootstrap();
--- backend/database/db.ts
+++ backend/database/db.ts
@@ -1,22 +1,18 @@
 import { MongoClient, Db } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 
-const uri: string = process.env.MONGODB_URI || 'mongodb://localhost:27017'
+const uri: string = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
-const dbName: string = 'EuroLingo'
+const dbName: string = "EuroLingo";
 
-export async function createDatabase (): Promise<Db> {
-    const client = new MongoClient(uri)
-    try {
-        await client.connect()
-        return client.db(dbName)
-    }
-    catch(err) {
-        console.log(err)
-    }
+export async function createDatabase(): Promise<Db> {
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    return client.db(dbName);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
-
-
-
-
-
-
