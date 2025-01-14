4b83f3acd69b7e66eeb3ee34534259825f667fa0
IIHHII <burhan.anjum2005@gmail.com>
2025-01-14 10:24:06 +0000

    fetch words

--- backend/controllers/app.controller.ts
+++ backend/controllers/app.controller.ts
@@ -1,6 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { fetchAllWord } from "../models/app.models";
 
 
 export const getApi = (req: Request, res: Response, next: NextFunction) => {
     res.send({ msg: 'hello' })
+}
+
+export const getAllWords = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const language : string = req.params.language
+        const words = await fetchAllWord(language)
+        res.status(200).send(words)
+    }
+    catch(err){
+        console.error(err)
+    }
+    
 }
