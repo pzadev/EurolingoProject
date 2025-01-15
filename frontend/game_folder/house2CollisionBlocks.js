4dae69599273794a83922b5b799290493d3cf35b
Peter Zak Austin <peterzaustin@icloud.com>
2025-01-15 08:42:49 +0000

    centred house scenes

--- frontend/game_folder/house2CollisionBlocks.js
+++ frontend/game_folder/house2CollisionBlocks.js
@@ -1,26 +1,24 @@
 export default class House2CollisionBlocks {
-    constructor(scene) {
-      this.scene = scene;
-      this.House2CollisionBlocks = scene.physics.add.staticGroup();
+  constructor(scene) {
+    this.scene = scene;
+    this.House2CollisionBlocks = scene.physics.add.staticGroup();
 
-      this.House2CollisionBlocks.create(55, 250, "collision")
+    this.House2CollisionBlocks.create(285, 250, "collision")
       .setSize(10, 500)
       .setOrigin(1, 1)
       .setVisible(false);
 
-      this.House2CollisionBlocks.create(300, 230, "collision")
+    this.House2CollisionBlocks.create(530, 230, "collision")
       .setSize(500, 10)
       .setOrigin(1, 1)
       .setVisible(false);
 
-      this.House2CollisionBlocks.create(300, 475, "collision")
+    this.House2CollisionBlocks.create(530, 475, "collision")
       .setSize(500, 10)
       .setOrigin(1, 1)
       .setVisible(false);
-
-
-    }
-      getHouse2Blocks() {
-        return this.House2CollisionBlocks;
-      }
-    }
+  }
+  getHouse2Blocks() {
+    return this.House2CollisionBlocks;
+  }
+}
